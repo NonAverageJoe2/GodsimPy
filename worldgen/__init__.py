0c6d0f77ca2135e1ca7b52299006f3237ee90966
NonAverageJoe2 <elevenlabs03@gmail.com>
2025-09-02 08:29:11 -0400

    Create __init__.py

---
+++ worldgen/__init__.py
@@ -0,0 +1,16 @@
+# worldgen/__init__.py
+# Package init for world generation modules
+
+from .hexgrid import axial_to_world_flat, neighbors_axial, in_bounds, idx
+from .noise import value_noise
+from .plates import generate_plates, apply_plate_forces
+from .biomes import build_biomes, GRASS, COAST, MOUNTAIN, OCEAN
+from .worldgen import build_world
+
+__all__ = [
+    "axial_to_world_flat", "neighbors_axial", "in_bounds", "idx",
+    "value_noise",
+    "generate_plates", "apply_plate_forces",
+    "build_biomes", "GRASS", "COAST", "MOUNTAIN", "OCEAN",
+    "build_world",
+]
